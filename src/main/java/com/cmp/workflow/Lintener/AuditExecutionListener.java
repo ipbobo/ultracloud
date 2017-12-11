77fb5f2ad4f9a1751a5b203159ba5683aa55940a
abcde0818 <abcde0818@163.com>
2017-12-11 20:38:23 +0800

    workflow listener

---
+++ src/main/java/com/cmp/workflow/Lintener/AuditExecutionListener.java
@@ -0,0 +1,21 @@
+package com.cmp.workflow.Lintener;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.ExecutionListener;
+
+/**
+ * 审核执行监听器
+ * 
+ * @author liuweixing
+ *
+ */
+public class AuditExecutionListener implements ExecutionListener {
+
+	private static final long serialVersionUID = 7559229982234452018L;
+
+	@Override
+	public void notify(DelegateExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+	}
+
+}
