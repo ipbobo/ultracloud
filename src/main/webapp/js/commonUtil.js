ffafd8c0dcce709c76e334b21a0dbd09db9a9b37
wsysr <wsysr@163.com>
2018-02-04 21:05:52 +0800

    修改bug

--- src/main/webapp/js/commonUtil.js
+++ src/main/webapp/js/commonUtil.js
@@ -97,15 +97,19 @@ function getPieChart(chartId, titleArr, legendArr, radiusArr, centerArr, keyArr,
 //获取环形图，必须先引入echarts.min.js
 function getAnnularChart(chartId, titleArr, legendArr, radiusArr, centerArr, keyArr, valArr, colorArr, unit){
 	var dataArr=[];
+	var isAllZero=true;//值是否全为0
 	if(keyArr.length==valArr.length && keyArr.length==colorArr.length){
 		for(var i=0;i<keyArr.length;i++){
 			dataArr.push({name: keyArr[i], value: valArr[i]});
+			if(valArr[i]!='0'){
+				isAllZero=false;
+			}
 		}
 	}
 	
 	var option = {
 		title: {text: titleArr[0], subtext: '', x:'center', y: '100', textStyle: {color: '#000000', fontSize: '12', fontWeight: 'normal'}},  
-	    tooltip: {trigger: 'item', formatter: "{a}<br>{b}: {c}({d}"+unit+")", textStyle:{align:'left'}},//item或axis
+	    tooltip: {trigger: 'item', formatter: "{b}("+(isAllZero?"0":"{d}")+unit+")", textStyle:{align:'left'}},//item或axis
 	    legend: {orient: legendArr[0], x: legendArr[1]?legendArr[1]:'center', y: legendArr[2]?legendArr[2]:'top', data: keyArr},//horizontal、vertical
 	    color: colorArr,
 	    calculable: true,  
